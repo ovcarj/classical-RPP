6133dba97414b36b6aae0457dfed79d25c7f1199
ovcarj <juraj.ovcar@gmail.com>
2020-05-22 14:22:15 +0200

    Old files cleanup

--- structure_maker/inorganic/perfect/MA_maker/traj_to_POSCAR.py
+++
@@ -1,8 +0,0 @@
-from ase.io import read, write
-import sys
-
-name = str(sys.argv[1])
-
-atoms = read(name)
-
-write('POSCAR' + name.split('.')[0], format='vasp', images=atoms)
--- structure_maker/inorganic/perfect/final/edit_cell.py
+++
@@ -1,9 +0,0 @@
-import numpy as np
-import sys
-from ase.io import read
-
-atoms = read(str(sys.argv[1]))
-
-atoms.cell[2][2] = np.max(atoms.positions[:,2])
-
-atoms.write(str(sys.argv[1]))
